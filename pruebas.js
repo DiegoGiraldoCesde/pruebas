a3693649da2e5714e76732f632544ccf7f305cd3
DiegoGiraldoZ <beargiraldo@gmail.com>
2026-03-04 07:24:50 -0500

    primer commit

---
+++ pruebas.js
@@ -0,0 +1,12 @@
+//función flecha
+
+const greetings = (name = 'Diego') => {
+  let message = name + ', welcome to 30 Days Of JavaScript!'
+  return message
+}
+
+console.log(greetings())
+console.log(greetings('Diego Giraldo'))
+
+
+
